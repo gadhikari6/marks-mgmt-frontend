e41a247bbc2cbf3b69a151bf0a556e59709b2fc3
Gaurab Adhikari <agaurabadhikari@gmail.com>
2024-05-31 13:31:53 +0545

    Added more login logic and readme.md

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 DO NOT merge in the main branch.
 
 We will be working on development branch.
+
+## NECESSARY TOOLS
+
+1. node, npm, pnpm
+2. Vscode
+3. Git
+
+## Run
+
+1. Git clone the directory
+2. Inside the directory `marks-mgmt-frontend`
+   <br/>Run:
+   ```bash
+   pnpm install
+   cp .env.example .env
+   ```
+3. Edit `.env` file as required
+4. Run:
+
+```bash
+   pnpm run dev
+```
+
+_Note: Run `pnpm run lint-fix` to fix auto-fixable linting problems_
--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -1,14 +1,8 @@
 import { useState } from "react"
 import { useFormik } from "formik"
 import * as yup from "yup"
-import {
-  TextField,
-  Checkbox,
-  Button,
-  FormControlLabel,
-  Box,
-  Typography,
-} from "@mui/material"
+import { TextField, Button, Box, Typography, Paper } from "@mui/material"
+const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL // fetching from .env file
 
 /**
  * LoginForm Component
@@ -17,7 +11,6 @@ const LoginForm = () => {
   const initialValues = {
     email: "",
     password: "",
-   
   }
 
   const [loginError, setLoginError] = useState("")
@@ -30,30 +23,31 @@ const LoginForm = () => {
     setLoginSuccess("")
 
     try {
-      const response = await fetch("http://localhost:9000/api/v1/login", {
+      const response = await fetch(`${VITE_BACKEND_URL}/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(values),
-      })
-      console.log(values)
-
-      const data = await response.json()
-
-      console.log(data)
+      }).catch((err) => console.log(err))
 
       if (response.ok) {
         // Login success
         setLoginSuccess("Login successful!")
-        console.log(data.token) // Access token
-      } else {
-        // Login error
+
+        const data = await response.json()
+        // Access token
+        console.log(data.token) // TODO: to remove later
+      } else if (response.status === 401) {
+        // 401 : Invalid credentails
         setLoginError("Invalid email or password")
+      } else {
+        console.error("Login failed:", response)
+        setLoginError("An error occurred during login. Please try again.")
       }
-    } catch (error) {
-      console.error("Login failed:", error)
-      setLoginError("An error occurred during login")
+    } catch (err) {
+      console.error("Login failed:", err) // Most probably, network problem
+      setLoginError("An error occurred during login. Please try again.")
     }
 
     setLoading(false)
@@ -64,7 +58,10 @@ const LoginForm = () => {
       .string()
       .email("Please enter a valid email address")
       .required("Email field is required"),
-    password: yup.string().required("Password field is required"),
+    password: yup
+      .string()
+      .min(5, "The minimum length of Password is 5 characters")
+      .required("Password field is required"),
   })
 
   const formik = useFormik({
@@ -80,39 +77,49 @@ const LoginForm = () => {
       alignItems="center"
       height="100vh"
     >
-      <form onSubmit={formik.handleSubmit}>
-        <Box maxWidth={400} width="100%" padding={2}>
-          <Typography variant="h4" align="center" gutterBottom>
-            Login
-          </Typography>
+      {/* TODO: Add banner or text mentioning IMMS , also some logo/icon for login */}
+      <Paper
+        variant="elevation"
+        elevation={2}
+        sx={{
+          padding: 2,
+          borderRadius: 1,
+          paddingBottom: 1,
+        }}
+      >
+        <form onSubmit={formik.handleSubmit}>
+          <Box maxWidth={400} width="100%" padding={2}>
+            <Typography variant="h4" align="center" gutterBottom>
+              Login
+            </Typography>
 
-          <TextField
-            id="email"
-            name="email"
-            label="Email"
-            type="email"
-            variant="outlined"
-            {...formik.getFieldProps("email")}
-            error={formik.touched.email && formik.errors.email}
-            helperText={formik.touched.email && formik.errors.email}
-            fullWidth
-            margin="normal"
-          />
+            <TextField
+              id="email"
+              name="email"
+              label="Email *"
+              type="email"
+              variant="outlined"
+              {...formik.getFieldProps("email")}
+              error={formik.touched.email && formik.errors.email}
+              helperText={formik.touched.email && formik.errors.email}
+              fullWidth
+              margin="normal"
+            />
 
-          <TextField
-            id="password"
-            name="password"
-            label="Password"
-            type="password"
-            variant="outlined"
-            {...formik.getFieldProps("password")}
-            error={formik.touched.password && formik.errors.password}
-            helperText={formik.touched.password && formik.errors.password}
-            fullWidth
-            margin="normal"
-          />
+            <TextField
+              id="password"
+              name="password"
+              label="Password *"
+              type="password"
+              variant="outlined"
+              {...formik.getFieldProps("password")}
+              error={formik.touched.password && formik.errors.password}
+              helperText={formik.touched.password && formik.errors.password}
+              fullWidth
+              margin="normal"
+            />
 
-          {/* <FormControlLabel
+            {/* <FormControlLabel
             control={
               <Checkbox
                 id="rememberMe"
@@ -124,30 +131,44 @@ const LoginForm = () => {
             label="Remember me"
           /> */}
 
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            fullWidth
-            disabled={loading}
-          >
-            {loading ? "Logging in..." : "Submit"}
-          </Button>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              fullWidth
+              disabled={loading}
+              sx={{ marginTop: 2 }}
+            >
+              {loading ? "Logging in..." : "Submit"}
+            </Button>
 
-          {loginError && (
-            <Typography color="error" align="center" paragraph>
-              {loginError}
-           
-            </Typography>
-          )}
+            {loginError && (
+              <Typography
+                sx={{ marginTop: 1 }}
+                color="error"
+                align="center"
+                paragraph
+              >
+                {loginError}
+              </Typography>
+            )}
 
-          {loginSuccess && (
-            <Typography color="success" align="center" paragraph>
-              {loginSuccess}
-            </Typography>
-          )}
-        </Box>
-      </form>
+            {loginSuccess && (
+              <Typography
+                sx={{ marginTop: 2 }}
+                color="green"
+                align="center"
+                paragraph
+              >
+                {loginSuccess}
+              </Typography>
+            )}
+
+            {/*
+            TODO: Add forgot password link */}
+          </Box>
+        </form>
+      </Paper>
     </Box>
   )
 }
