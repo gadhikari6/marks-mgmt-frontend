89b0a4b6c40946b2d2964094b784c49bc8506226
Gaurab Adhikari <agaurabadhikari@gmail.com>
2024-06-23 13:32:18 +0545

    added icon for add student option in sidebar

--- src/components/sidebar/AdminDrawer.jsx
+++ src/components/sidebar/AdminDrawer.jsx
@@ -23,7 +23,7 @@ export default function AdminDrawer() {
 
           { text: "Profile", icon: <AccountBoxIcon />, link: "/profile" },
           { text: "Add Teacher", icon: <PersonAddIcon />, link: "/addTeacher" },
-          { text: "Add Student", link: "/addStudent" },
+          { text: "Add Student", icon: <PersonAddIcon />, link: "/addStudent" },
         ].map((item) => (
           <ListItem
             key={item.text}
